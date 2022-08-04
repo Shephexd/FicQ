61824d9cb90abfce51d7f2b61b2d57e97085355e
Han <taekyung@b0be83732d81.ant.amazon.com>
2022-08-04 10:49:02 +0900

    ADD FinQ demo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { HomePage } from "./pages/home";
+import { NewsPage } from "./pages/news";
+import { WordCloudPage } from "./pages/wordcloud";
+import "antd/dist/antd.css";
+import { Layout, Menu } from "antd";
+
+import {
+  Routes,
+  Route,
+  Link,
+  BrowserRouter
+} from 'react-router-dom';
+const {Header, Content, Footer} = Layout;
+
+
+const menuItems = [
+  {
+    label: <Link to="/">Home</Link>,
+    key: "home"
+  },
+  {
+    label: <Link to="/news">News</Link>,
+    key: "news"
+  },
+  {
+    label: <Link to="/wc">WordCloud</Link>,
+    key: "wc"
+  }
+]
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Layout style={{ height: '100vh' }}>
+        <BrowserRouter>        
+            <Menu mode="horizontal" items={menuItems}/>
+            
+          <Content style={{ padding: '0 50px', height: '85vh'}}>
+              <Routes>
+              <Route path="/" element={<HomePage/>} />
+              <Route path="/news" element={<NewsPage/>} />
+              <Route path="/wc" element={<WordCloudPage/>} />
+            </Routes>
+          </Content>
+          </BrowserRouter>
+        <Footer>FinQ created By AnyCompany, 2022</Footer>
+      </Layout>
+    </div>      
   );
 }
 
---
+++ src/pages/wordcloud.tsx
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react';
+import { WordCloud } from '@ant-design/plots';
+
+
+const WordCloudPage = () => {
+    const [data, setData] = useState([]);
+  
+    useEffect(() => {
+      if(data.length == 0)
+        asyncFetch();
+    }, [data]);
+  
+    
+    const asyncFetch = () => {
+      fetch('https://7ch64e1bgc.execute-api.ap-northeast-2.amazonaws.com/dev/api/analytics/wc/topics')
+        .then((response) => response.json())
+        .then((json) => setData(json))
+        .catch((error) => {
+          console.log('fetch data failed', error);
+        });
+    };
+    const config = {
+      data,
+      autoFit: false,
+      wordField: 'text',
+      weightField: 'value',
+      colorField: 'text',
+      wordStyle: {
+        fontFamily: 'Verdana',
+      }
+    };
+    return <WordCloud {...config} />;
+  };
+
+  export {WordCloudPage};
